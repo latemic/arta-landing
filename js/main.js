3a170ce3258f5195a1407322bdf82fb01535a8c8
Turv93 <erlang871@gmail.com>
2018-02-20 15:49:02 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,3 +9,28 @@ $('.navbar').removeClass('navbar-fix');
 
 });
 
+var modal = document.getElementById('simpleModal');
+
+var modalBtn = document.getElementById('modalBtn');
+
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+
+
+modalBtn.addEventListener('click', openModal);
+
+
+closeBtn.addEventListener('click', closeModal);
+
+
+window.addEventListener('click', clickOutside);
+
+function openModal(){
+    modal.style.display = 'block';
+}
+
+
+function closeModal(){
+    modal.style.display = 'none';
+}
+
