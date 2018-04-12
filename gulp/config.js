a6c722d35ae3739164c14121ac85198c17360579
Vadym Selishchev <vadym@temy.co>
2018-04-12 13:41:24 +0300

    Fixed config paths.

--- gulp/config.js
+++ gulp/config.js
@@ -1,30 +1,30 @@
 'use strict';
 
-const src = `./src`;
-const dist = `./dist`;
+const src = `./src/`;
+const dist = `./dist/`;
 
 module.exports = {
   paths: {
     fonts: {
-      src: `${src}/fonts/**/*.woff2`,
-      dest: `${dist}/fonts`
+      src: src + 'fonts/**/*.woff2',
+      dest: dist + 'fonts'
     },
     styles: {
-      src: `${src}/scss/**/*.scss`,
-      dest: `${dist}/css`
+      src: src + 'scss/**/*.scss',
+      dest: dist + 'css'
     },
     images: {
-      src: `${src}/images/**/*`,
-      dest: `${dist}/img`
+      src: src + 'images/**/*',
+      dest: dist + 'img'
     },
     svg: {
-      src: `${src}/svg/*.svg`,
-      dest: `${dist}/svg`
+      src: src + 'svg/*.svg',
+      dest: dist + 'svg'
     },
     markup: {
-      templates: `${src}/njk/*.njk`,
-      path: `${src}/njk/`,
-      src: `${src}/njk/**/*.njk`,
+      templates: src + 'njk/*.njk',
+      path: src + 'njk/',
+      src: src + 'njk/**/*.njk',
       dest: dist
     },
     dist: dist
