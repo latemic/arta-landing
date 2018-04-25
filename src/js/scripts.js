1712e98e3471ff5ce407ab9660234771b762f82d
OleksandrBugor <alexanderbugor@gmail.com>
2018-04-25 17:33:51 +0300

    Added subscribe modal window

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,22 @@
 const babel = 'babel';
 console.log('scripts has loaded!');
 console.log(`${babel} works!`);
+
+
+//SUBSCRIBE WINDOW
+var modal = document.getElementById('sub');
+var btn = document.getElementById('modal-window');
+var close = document.getElementById('close-modal-window');
+
+btn.addEventListener('click', openModalWindow);
+close.addEventListener('click', closeModalWindow);
+
+//Open Modal Window
+function openModalWindow() {
+  modal.classList.add('subscribe--visible');
+}
+
+//Close Modal Window
+function closeModalWindow() {
+  modal.classList.remove('subscribe--visible');
+}
