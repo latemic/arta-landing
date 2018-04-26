225feb3619a9157943d1a5a932eef456c2c84425
OleksandrBugor <alexanderbugor@gmail.com>
2018-04-26 15:35:26 +0300

    Added Mobile Navigation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -33,3 +33,30 @@ window.onscroll = function changeClass(){
     navBar.classList.remove('header--scrolled');
   }
 }
+
+//MOBILE MENU
+var menu = document.getElementById('menu');
+var burger = document.getElementById('toggle-menu');
+var closeMenuBtn = document.getElementById('close-menu');
+
+burger.addEventListener('click', openMenu);
+closeMenuBtn.addEventListener('click', closeMenu);
+
+//Open Menu
+function openMenu(){
+  menu.classList.add('mobile-menu--visible');
+}
+
+//Close Menu
+function closeMenu(){
+  menu.classList.remove('mobile-menu--visible');
+}
+
+//Any click outside of the Menu will close the menu if it's open
+document.addEventListener('click', function(event) {
+  var isClickInside = menu.contains(event.target);
+  var isToggleClicked = burger.contains(event.target);
+  if (!isClickInside && !isToggleClicked) {
+    closeMenu();
+  }
+});
