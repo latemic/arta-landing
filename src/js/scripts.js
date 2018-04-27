5244e11a298277504f9814a9042c4a98d996d39b
OleksandrBugor <alexanderbugor@gmail.com>
2018-04-27 13:10:16 +0300

    Added script for scrolling invoked by arrows

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -12,13 +12,15 @@ btn.addEventListener('click', openModalWindow);
 close.addEventListener('click', closeModalWindow);
 
 //Open Modal Window
-function openModalWindow() {
+function openModalWindow(e) {
   modal.classList.add('subscribe--visible');
+  e.preventDefault();
 }
 
 //Close Modal Window
-function closeModalWindow() {
+function closeModalWindow(e) {
   modal.classList.remove('subscribe--visible');
+  e.preventDefault();
 }
 
 
@@ -65,3 +67,118 @@ document.addEventListener('click', function(e) {
 
 
 //FULL PAGE SCROLLING
+function scrollIt(destination, duration = 200, easing = 'linear', callback) {
+
+  const easings = {
+    linear(t) {
+      return t;
+    },
+    easeInQuad(t) {
+      return t * t;
+    },
+    easeOutQuad(t) {
+      return t * (2 - t);
+    },
+    easeInOutQuad(t) {
+      return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+    },
+    easeInCubic(t) {
+      return t * t * t;
+    },
+    easeOutCubic(t) {
+      return (--t) * t * t + 1;
+    },
+    easeInOutCubic(t) {
+      return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+    },
+    easeInQuart(t) {
+      return t * t * t * t;
+    },
+    easeOutQuart(t) {
+      return 1 - (--t) * t * t * t;
+    },
+    easeInOutQuart(t) {
+      return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;
+    },
+    easeInQuint(t) {
+      return t * t * t * t * t;
+    },
+    easeOutQuint(t) {
+      return 1 + (--t) * t * t * t * t;
+    },
+    easeInOutQuint(t) {
+      return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
+    }
+  };
+
+  const start = window.pageYOffset;
+  const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+
+  const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+  const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
+  const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
+
+  if ('requestAnimationFrame' in window === false) {
+    window.scroll(0, destinationOffsetToScroll);
+    if (callback) {
+      callback();
+    }
+    return;
+  }
+
+  function scroll() {
+    const now = 'now' in window.performance ? performance.now() : new Date().getTime();
+    const time = Math.min(1, ((now - startTime) / duration));
+    const timeFunction = easings[easing](time);
+    window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
+
+    if (window.pageYOffset === destinationOffsetToScroll) {
+      if (callback) {
+        callback();
+      }
+      return;
+    }
+
+    requestAnimationFrame(scroll);
+  }
+
+  scroll();
+};
+
+//Invoking Scrolling on arrows
+document.querySelector('.intro__arrow').addEventListener('click', () => {
+  scrollIt(
+    document.querySelector('.one-stop'),
+    300,
+    'easeOutQuad',
+    () => console.log(`Just finished scrolling to ${window.pageYOffset}px`)
+  );
+});
+
+document.querySelector('.realistic-preview__arrow').addEventListener('click', () => {
+  scrollIt(
+    document.querySelector('.mobile-sdk'),
+    300,
+    'easeOutQuad',
+    () => console.log(`Just finished scrolling to ${window.pageYOffset}px`)
+  );
+});
+
+document.querySelector('.assets__arrow').addEventListener('click', () => {
+  scrollIt(
+    document.querySelector('.ar-cloud'),
+    300,
+    'easeOutQuad',
+    () => console.log(`Just finished scrolling to ${window.pageYOffset}px`)
+  );
+});
+
+document.querySelector('.lamp__arrow').addEventListener('click', () => {
+  scrollIt(
+    document.querySelector('.visualisation'),
+    300,
+    'easeOutQuad',
+    () => console.log(`Just finished scrolling to ${window.pageYOffset}px`)
+  );
+});
