3dabdd64781dcdc511eeccf6bcdf4e9c4b9f7c4e
Vadym Selishchev <vadym@temy.co>
2018-04-27 17:02:14 +0300

    Updated gulp config.

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -5,6 +5,6 @@ const del = require('del');
 const config = require('../config');
 
 gulp.task('fonts', function() {
-  return gulp.src(config.paths.fonts.src)
+  return gulp.src([config.paths.fonts.src.woff, config.paths.fonts.src.woff2])
     .pipe(gulp.dest(config.paths.fonts.dest))
 });
