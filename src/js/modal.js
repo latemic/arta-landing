31bbacbf76d7785d8ab461e076ecd492ca43702a
OleksandrBugor <alexanderbugor@gmail.com>
2018-05-22 18:11:38 +0300

    Added script for Mobile Menu

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,77 +1,120 @@
-//SUBSCRIBE WINDOW
-let modal = document.getElementById('sub');
-let btn = document.getElementById('open-modal-window');
-let close = document.getElementById('close-modal-window');
+'use strict';
 
-btn.addEventListener('click', openModalWindow);
-close.addEventListener('click', closeModalWindow);
+class ModalWindow {
+  constructor(parameters) {
+    this.toggles = document.getElementsByClassName(parameters.toggleSelector);
+    this.toggleSelector = parameters.toggleSelector;
+    this.visibility = parameters.visibility;
+    this.modal = parameters.modal;
 
-function addClass(obj, className) {
-  obj.classList.add(className);
-}
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+    this.handleMobileMenuClick = this.handleMobileMenuClick.bind(this);
+    this.hide = this.hide.bind(this);
 
-function removeClass(obj, className) {
-  obj.classList.remove(className);
-}
+    this.init();
+  }
 
-function defaultBehaviour(e, flowStyle) {
-  e.preventDefault();
-  document.body.style.overflow = flowStyle;
-}
+  init() {
+    this.setToggleEvents();
 
-//Open Modal Window
-function openModalWindow(e) {
-  defaultBehaviour(e, 'hidden');
-  addClass(modal, 'subscribe--visible');
-}
+    document.addEventListener('click', this.hide);
+  }
 
-//Close Modal Window
-function closeModalWindow(e) {
-  defaultBehaviour(e, 'auto');
-  removeClass(modal, 'subscribe--visible');
-}
+  setToggleEvents() {
+    if (!this.toggles) return;
 
+    for(let i = 0; i < this.toggles.length; i++) {
+      this.toggles[i].addEventListener('click', this.handleToggleClick);
+      this.setMobileMenuEvents(this.toggles[i]);
+    }
+  }
 
-//CHANGING HEADER CLASS ON SCROLLING
-window.addEventListener('scroll', changeHeaderClass);
+  setMobileMenuEvents() {
+    const mobileMenu = document.getElementById('menu');
+    const closeModal = document.getElementById('close-menu');
+    closeModal.addEventListener('click', this.hide);
+    mobileMenu.addEventListener('click', this.handleMobileMenuClick);
+  }
 
-function changeHeaderClass(){
-  let scrollPosY = window.pageYOffset | document.body.scrollTop;
-  let navBar = document.getElementById('changing-header');
+  handleToggleClick(event) {
+    event.stopPropagation();
 
-  if(scrollPosY > 74) {
-    addClass(navBar, 'header--scrolled');
-  } else if(scrollPosY <= 74) {
-    removeClass(navBar, 'header--scrolled');
+    const toggle = event.target;
+    const isTarget = toggle.classList.contains(this.toggleSelector);
+    const modalLayout = document.getElementById(this.modal);
+
+    if (!isTarget) return;
+
+    modalLayout.classList.add(this.visibility);
+  }
+
+  handleMobileMenuClick(event) {
+    event.stopPropagation();
+  }
+
+  hide() {
+    const modalLayout = document.getElementById(this.modal);
+      modalLayout.classList.remove(this.visibility);
   }
 }
 
+const modalWindow = new ModalWindow({
+  toggleSelector: 'toggle-menu',
+  visibility: 'item--visible',
+  modal: 'menu'
+});
 
-// //MOBILE MENU
-// let menu = document.getElementById('menu');
-// let burger = document.getElementById('toggle-menu');
-// let closeMenuBtn = document.getElementById('close-menu');
+
+// //SUBSCRIBE WINDOW
+// (function (){
 //
-// burger.addEventListener('click', openMenu);
-// closeMenuBtn.addEventListener('click', closeMenu);
+//   'use strict';
 //
-// //Open Menu
-// function openMenu(e){
-//   defaultBehaviour(e, 'hidden');
-//   addClass(menu, 'mobile-menu--visible');
-// }
+//   const modal = document.getElementById('sub');
+//   let btn = document.getElementById('open-modal-window');
+//   let close = document.getElementById('close-modal-window');
 //
-// //Close Menu
-// function closeMenu(e){
-//   defaultBehaviour(e, 'auto');
-//   removeClass(menu, 'mobile-menu--visible');
-// }
+//   btn.addEventListener('click', openModalWindow);
+//   close.addEventListener('click', closeModalWindow);
 //
-// //Any click outside of the Menu will close the menu
-// document.addEventListener('click', function(e) {
-//   let isClickInside = menu.contains(e.target);
-//   let isToggleClicked = burger.contains(e.target);
-//   if (!isClickInside && !isToggleClicked) {
-//     removeClass(menu, 'mobile-menu--visible');
+//   function addClass(obj, className) {
+//     obj.classList.add(className);
 //   }
-// });
+//
+//   function removeClass(obj, className) {
+//     obj.classList.remove(className);
+//   }
+//
+//   function defaultBehaviour(e, flowStyle) {
+//     e.preventDefault();
+//     document.body.style.overflow = flowStyle;
+//   }
+//
+//   //Open Modal Window
+//   function openModalWindow(e) {
+//     defaultBehaviour(e, 'hidden');
+//     addClass(modal, 'subscribe--visible');
+//   }
+//
+//   //Close Modal Window
+//   function closeModalWindow(e) {
+//     defaultBehaviour(e, 'auto');
+//     removeClass(modal, 'subscribe--visible');
+//   }
+//
+//
+//   //CHANGING HEADER CLASS ON SCROLLING
+//   window.addEventListener('scroll', changeHeaderClass);
+//
+//   function changeHeaderClass(){
+//     let scrollPosY = window.pageYOffset | document.body.scrollTop;
+//     let navBar = document.getElementById('changing-header');
+//
+//     if(scrollPosY > 74) {
+//       addClass(navBar, 'header--scrolled');
+//     } else if(scrollPosY <= 74) {
+//       removeClass(navBar, 'header--scrolled');
+//     }
+//   }
+//
+// })();
