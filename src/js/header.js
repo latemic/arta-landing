59fd9b781355067aeee1abdf6d51b5c4ad362d38
OleksandrBugor <alexanderbugor@gmail.com>
2018-05-23 10:42:20 +0300

    Added script for scrolling

---
+++ src/js/header.js
@@ -0,0 +1,18 @@
+//CHANGING HEADER CLASS ON SCROLLING
+
+(function() {
+  'use strict';
+
+  window.addEventListener('scroll', changeHeaderClass);
+
+  function changeHeaderClass(){
+    let scrollPosY = window.pageYOffset | document.body.scrollTop;
+    const navBar = document.getElementById('changing-header');
+
+    if(scrollPosY > 74) {
+      navBar.classList.add('header--scrolled');
+    } else if(scrollPosY <= 74) {
+      navBar.classList.remove('header--scrolled');
+    }
+  };
+})();
